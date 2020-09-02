56615da2ac4e8e32c89e540fa95f29482a93c2fe
Mightynumnums <deadcookies@gmail.com>
2020-09-02 11:44:56 -0400

    Update comments

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,71 +1,67 @@
-
-//Checks to see that each Row has numbers 1-9 with no repeats
-
+//Gets row
 const getRow = (puzzle, rowNum) => {
-  return puzzle[rowNum]
+    return puzzle[rowNum]
 }
 
-//Checks to see that each column has numbers 1-9 with no repeats
-
+//Gets a column
 const getColumn = (puzzle, colNum) => {
-  let colArr = []
-  for (let i = 0; i < puzzle.length; i++) {
-    colArr.push(puzzle[i][colNum])
-  }
-  return colArr
+    let colArr = []
+    for (let i = 0; i < puzzle.length; i++) {
+        colArr.push(puzzle[i][colNum])
+    }
+    return colArr
 }
 
+//Gets a 3x3 section based on x and y coordinates
 function getSection(puzzle, xCoord, yCoord) {
-  let section = []
-  xCoord  *= 3
-  yCoord *= 3
+    let section = []
+    xCoord *= 3
+    yCoord *= 3
 
-  for (let i = yCoord; i < (yCoord + 3); i++) {
-    for (let j = xCoord ; j < (xCoord + 3); j++) {
-      section.push(puzzle[i][j])
+    for (let i = yCoord; i < yCoord + 3; i++) {
+        for (let j = xCoord; j < xCoord + 3; j++) {
+            section.push(puzzle[i][j])
+        }
     }
-  }
-  return section
+    return section
 }
 
 // Check to see that each section (row, col or section, include all nums 1-9)
-
 function includes1to9(section) {
-  let val = false
-  for( let i = 1; i <= section.length; i++) {
-    if (section.indexOf(i) === -1) {
-      return val
+    let val = false
+    for (let i = 1; i <= section.length; i++) {
+        if (section.indexOf(i) === -1) {
+            return val
+        }
     }
-  }
-  return !val
+    return !val
 }
 
 //Check to see that the entire sudoku is valid, IE each row, col, and section all have numbers 1-9 with no repeats
 
 function sudokuIsValid(puzzle) {
-	let checksArr = []
-	for (let i = 0; i < 9; i++) {
-		checksArr.push(getRow(puzzle, i))
-		checksArr.push(getColumn(puzzle, i))
-	}
-	 for(let i = 0; i < 3; i++) {
-    for(let j = 0; j < 3; j++) {
-      checksArr.push(getSection(puzzle, i, j));
+    let checksArr = []
+    for (let i = 0; i < 9; i++) {
+        checksArr.push(getRow(puzzle, i))
+        checksArr.push(getColumn(puzzle, i))
     }
-  }
-	for(let i = 0; i < checksArr.length; i++) {
-    if(!includes1to9(checksArr[i])) {
-			 return false;
-		}
-  }
-	return true
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            checksArr.push(getSection(puzzle, i, j))
+        }
+    }
+    for (let i = 0; i < checksArr.length; i++) {
+        if (!includes1to9(checksArr[i])) {
+            return false
+        }
+    }
+    return true
 }
 
-
 module.exports = {
-  getRow,
-  getColumn,
-  getSection,
-  includes1to9,
-  sudokuIsValid
+    getRow,
+    getColumn,
+    getSection,
+    includes1to9,
+    sudokuIsValid,
 }
